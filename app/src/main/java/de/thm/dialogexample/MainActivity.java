9d86b4445bc44690693beb33a1f1165db2ed59a0
Yannick Bals <yannick.bals@web.de>
2018-05-03 09:22:23 +0200

    Add layout and Activity

---
+++ app/src/main/java/de/thm/dialogexample/MainActivity.java
@@ -0,0 +1,15 @@
+package de.thm.dialogexample;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Yannick Bals on 03.05.2018.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    
+
+
+
+}
